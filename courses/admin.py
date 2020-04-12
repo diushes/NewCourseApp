16b57a34aebde4323482b808c92edf6f39e77903
Diushes <diushes1712@gmail.com>
2020-04-13 00:38:36 +0600

    first commit

---
+++ courses/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import Branch, Course, Category,Contact
+
+admin.site.register(Branch)
+admin.site.register(Course)
+admin.site.register(Category)
+admin.site.register(Contact)
+
+
+
