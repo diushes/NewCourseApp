3687745ed94b7b5fd11cb03daad01bc02d3c92b4
Diushes <diushes1712@gmail.com>
2020-04-17 22:27:35 +0600

    commit

--- courseproject/settings.py
+++ courseproject/settings.py
@@ -20,12 +20,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'x2gsf8_ely&ia(s+^usv&zr^r_qv539)ja*t0d)*5q7bptu1(e'
+#SECRET_KEY = 'x2gsf8_ely&ia(s+^usv&zr^r_qv539)ja*t0d)*5q7bptu1(e'
+import os
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'x2gsf8_ely&ia(s+^usv&zr^r_qv539)ja*t0d)*5q7bptu1(e')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+#DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', False) )
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
