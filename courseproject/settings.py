213b43a553b65aa6240d88d2596be0feaba43569
Diushes <diushes1712@gmail.com>
2020-04-19 14:32:26 +0600

    added requirements

--- courseproject/settings.py
+++ courseproject/settings.py
@@ -20,14 +20,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'x2gsf8_ely&ia(s+^usv&zr^r_qv539)ja*t0d)*5q7bptu1(e'
-import os
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'x2gsf8_ely&ia(s+^usv&zr^r_qv539)ja*t0d)*5q7bptu1(e')
+SECRET_KEY = 'x2gsf8_ely&ia(s+^usv&zr^r_qv539)ja*t0d)*5q7bptu1(e'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = True
-DEBUG = bool( os.environ.get('DJANGO_DEBUG', False) )
-ALLOWED_HOSTS = ['*']
+DEBUG = True
 
 
 # Application definition
@@ -45,7 +42,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -125,18 +121,3 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-
-# Heroku: Update database configuration from $DATABASE_URL.
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.10/howto/static-files/
-
-# The absolute path to the directory where collectstatic will collect static files for deployment.
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-# The URL to use when referring to static files (where they will be served from)
-STATIC_URL = '/static/'
